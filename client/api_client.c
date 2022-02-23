36630740ef9400370435599ddbae9cd3ab2bdc89
Flavio Carrara De Capua <flavio@carrara.us>
2022-02-23 14:05:32 -0300

    argv[0] = IPV4:PORT check

--- client/api_client.c
+++ client/api_client.c
@@ -6,7 +6,7 @@
 /*   By: fde-capu <fde-capu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 11:45:14 by fde-capu          #+#    #+#             */
-/*   Updated: 2022/02/23 13:24:47 by fde-capu         ###   ########.fr       */
+/*   Updated: 2022/02/23 14:04:26 by fde-capu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,20 @@
 
 int main(int argc, char *argv[])
 {
-	char *valid_arguments_regex_pipesv = "\\d\\d?\\d?\\.\\d\\d?\\d?\\.\\d\\d?\\d?\\.\\d\\d?\\d?";
+	char *valid_arguments_regex_pipesv = "\\d\\d?\\d?\\.\\d\\d?\\d?\\.\\d\\d?\\d?\\.\\d\\d?\\d?:\\d+";
 	if (!validate_args_regex(argc, argv, 2, 2, valid_arguments_regex_pipesv))
 		return ft_print2_and_return_int(ERROR, INVALID_ARGUMENTS, 42);
 
-	char *address = argv[1];
+	char **addr_and_port = ft_split(argv[1], ':');
+	char *address = addr_and_port[0];
+	int port = ft_atoi(addr_and_port[1]);
+
 	int client_socket;
 	client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in remote_address;
 	remote_address.sin_family = AF_INET;
-	remote_address.sin_port = htons(8001);
+	remote_address.sin_port = htons(port);
 	inet_aton(address, (struct in_addr *)&remote_address.sin_addr.s_addr);
 
 	connect(client_socket, (struct sockaddr *)&remote_address, sizeof(remote_address));
